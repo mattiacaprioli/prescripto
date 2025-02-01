3fcb6ccb06f9c5c6f0c43375bcdbe4a1548f9989
Mattia Caprioli <capriolimattia1994@gmail.com>
2025-02-01 10:29:41 +0100

    feat doctorList admin Style and api changed available in back and front

--- admin/src/pages/Admin/DoctorsList.jsx
+++ admin/src/pages/Admin/DoctorsList.jsx
@@ -1,8 +1,40 @@
+import { useContext, useEffect } from "react";
+import { AdminContext } from "../../context/AdminContext";
 
-function DoctorsList() {
+const DoctorsList = () => {
+
+  const {doctors, atoken, getAllDoctors, changeAvailability} = useContext(AdminContext);
+
+  useEffect(() => {
+    if(atoken){
+      getAllDoctors();
+    }
+  }, [atoken])
+
+  console.log("doctors in file:",doctors);
+
   return (
-    <div>
-      DoctorsList
+    <div className="m-5 max-h-[90vh] overflow-y-scroll">
+      <h1 className="text-lg font-medium">All Doctors</h1>
+      <div className="w-full flex flex-wrap gap-4 pt-5 gap-y-6">
+        {
+          doctors.map((item, index) => {
+            return (
+              <div className="border border-indigo-200 rounded-xl max-w-56 overflow-hidden cursor-pointer group" key={index}>
+                <img className="bg-indigo-50 group-hover:bg-primary transition-all duration-500" src={item.image} alt="" />
+                <div className="p-4">
+                  <p className="text-neutral-800 text-lg font-medium">{item.name}</p>
+                  <p className="text-zinc-600 text-sm">{item.speciality}</p>
+                  <div className="mt-2 flex items-center gap-1 text-sm">
+                    <input onChange={() => changeAvailability(item._id)} type="checkbox" checked={item.available} />
+                    <p>available</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })          
+        }
+      </div>
     </div>
   )
 }
