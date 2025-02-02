7cbee1622b6ae085a71258d2a1adf78d4b7d8f68
Mattia Caprioli <capriolimattia1994@gmail.com>
2025-02-02 11:53:41 +0100

    create user login, token

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -9,12 +9,9 @@ const AppContextProvider = (props) => {
 
   const currencySymbol = "$";
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
-  const [doctors, setDoctors] = useState([]);
 
-  const value = {
-    doctors,
-    currencySymbol
-  }
+  const [doctors, setDoctors] = useState([]);
+  const [token, setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false);
 
   const getDoctorsData = async () => {
     try {
@@ -29,6 +26,14 @@ const AppContextProvider = (props) => {
       console.log(error);
       toast.error(error.message);
     }
+  }
+
+  const value = {
+    doctors,
+    currencySymbol,
+    token,
+    setToken,
+    backendUrl,
   }
 
   useEffect(() => {
