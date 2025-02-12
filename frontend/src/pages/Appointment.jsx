0be0008230643082342c1ba1513275de6dd726f1
Mattia Caprioli <capriolimattia1994@gmail.com>
2025-02-12 17:53:47 +0100

    finish api appointment

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -52,11 +52,22 @@ function Appointment() {
     while(currentDate < endTime){
       let formattedDate = currentDate.toLocaleString([], {hour: '2-digit', minute: '2-digit'});
 
-      // add slot to array
-      timeSlots.push({
-        datatime: new Date(currentDate),
-        time: formattedDate
-      });
+      let day = currentDate.getDate();
+      let month = currentDate.getMonth() + 1;
+      let year = currentDate.getFullYear();
+
+      const slotDate = day + "_" + month + "_" + year;
+      const slotTime = formattedDate;
+
+      const isSlotAvailable = docInfo.slots_booked[slotDate] && docInfo.slots_booked[slotDate].includes(slotTime) ? false : true;
+
+      if(isSlotAvailable){
+        // add slot to array
+        timeSlots.push({
+          datatime: new Date(currentDate),
+          time: formattedDate,
+        });
+      }
 
       // incrementing current time by 30 minutes
       currentDate.setMinutes(currentDate.getMinutes() + 30);
