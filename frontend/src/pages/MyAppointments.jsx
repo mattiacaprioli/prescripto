bdcdc5f4de9fa74a3f269a7305079e4d1f2d107e
Mattia Caprioli <capriolimattia1994@gmail.com>
2025-02-21 17:26:23 +0100

    feat button is paid

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -51,7 +51,7 @@ const MyAppointments = () => {
       console.log(error);
       toast.error(error.message);
     }
-  }
+  };
 
   // Funzione per iniziare il pagamento
   const handlePayOnline = async (appointmentId) => {
@@ -102,7 +102,8 @@ const MyAppointments = () => {
             </div>
             <div></div>
             <div className="flex flex-col gap-2 justify-end">
-              {!item.cancelled && <button onClick={() => handlePayOnline(item)} className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300">Pay Online</button>}
+              {!item.cancelled &&  item.payment && <button className="sm:min-w-48 py-2 border rounded text-stone-500 bg-indigo-50">Paid</button>}
+              {!item.cancelled && !item.payment && <button onClick={() => handlePayOnline(item)} className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-primary hover:text-white transition-all duration-300">Pay Online</button>}
               {!item.cancelled && <button onClick={() => cancelAppointment(item._id)} className="text-sm text-stone-500 text-center sm:min-w-48 py-2 border rounded hover:bg-red-600 hover:text-white transition-all duration-300">Cancel appointment</button>}
               {item.cancelled && <button className="sm:min-w-48 py-2 border border-red-500 rounded text-red-500">Appointment cancelled</button>}
             </div>
