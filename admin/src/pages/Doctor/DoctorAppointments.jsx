5754ee2a60ebca08bd72e0049f558c2e800beb56
Mattia Caprioli <capriolimattia1994@gmail.com>
2025-03-18 20:46:50 +0100

    feat doctor pages and token

---
+++ admin/src/pages/Doctor/DoctorAppointments.jsx
@@ -0,0 +1,19 @@
+import React, { useContext, useEffect } from 'react'
+import { DoctorContext } from '../../context/DoctorContext';
+
+function DoctorAppointments() {
+  const { dToken, appointments, getAppointments } = useContext(DoctorContext);
+
+  useEffect(() => {
+    if(dToken){
+      getAppointments();
+    }
+  }, [dToken]);
+  return (
+    <div>
+      DoctorAppointments
+    </div>
+  )
+}
+
+export default DoctorAppointments
