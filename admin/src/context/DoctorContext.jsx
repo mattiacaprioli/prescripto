fb1a9436b4f4f687c1a2efc277ebacd6d94ba365
Mattia Caprioli <capriolimattia1994@gmail.com>
2025-03-19 17:40:52 +0100

    doctor dash front end

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -9,6 +9,7 @@ const DoctorContextProvider = (props) => {
 
   const [dToken, setDToken] = useState(localStorage.getItem("dToken") ? localStorage.getItem("dToken") : "");
   const [appointments, setAppointments] = useState([]);
+  const [dashData, setDashData] = useState(false);
 
   const getAppointments = async () => {
     try {
@@ -61,6 +62,24 @@ const DoctorContextProvider = (props) => {
     }
   };
 
+  const getDashData = async () => {
+
+    try {
+      const { data } = await axios.get(backendUrl + "api/doctor/dashboard", { headers: { dToken } });
+
+      if (data.success) {
+        setDashData(data.dashData);
+        console.log(data.dashData);
+      } else {
+        toast.error(data.message);
+      }
+
+    } catch (error) {
+      console.error(error);
+      toast.error(error.message);
+    }
+  }
+
   const value = {
     dToken,
     setDToken,
@@ -69,7 +88,10 @@ const DoctorContextProvider = (props) => {
     setAppointments,
     getAppointments,
     completeAppointment,
-    cancelAppointment
+    cancelAppointment,
+    dashData,
+    setDashData,
+    getDashData
   };
 
   return (
